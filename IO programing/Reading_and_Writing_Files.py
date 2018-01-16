0ecbc14e09681b0e148682d1d4f13b83745c0c15
dxxZhangxq <lumiaxap@live.com>
2018-01-16 19:59:32 +0800

    io test

--- IO programing/Reading_and_Writing_Files.py
+++ IO programing/Reading_and_Writing_Files.py
@@ -5,15 +5,15 @@
 # @File    : Reading_and_Writing_Files.py
 # @Software: PyCharm
 
-with open('/home/zhangxq/Documents/Python/test.png', 'rb') as test_1:
+with open('/home/zhangxq/Documents/Python/py_study/test.png', 'rb') as test_1:
     print(test_1.read())
 print('\\\\\\\\\\\\\\\\\\\\\\\\')
-with open('/home/zhangxq/Documents/Python/test.txt', 'r', encoding='ascii', errors='ignore') as test_2:
+with open('/home/zhangxq/Documents/Python/py_study/test.txt', 'r', encoding='ascii', errors='ignore') as test_2:
     print(test_2.read())
 print('\\\\\\\\\\\\\\\\\\\\\\\\')
-with open('/home/zhangxq/Documents/Python/test.txt', 'r', encoding='gbk', errors='ignore') as test_3:
+with open('/home/zhangxq/Documents/Python/py_study/test.txt', 'r', encoding='gbk', errors='ignore') as test_3:
     print(test_3.read())
-with open('/home/zhangxq/Documents/Python/test_write', 'w', encoding='utf- 8') as test_4:
-    test_4.write('fuck you')
-with open('/home/zhangxq/Documents/Python/test.txt', 'a') as test_5:
-    test_5.write('fuck off')
+with open('/home/zhangxq/Documents/Python/py_study/test_write', 'a', encoding='utf- 8') as test_4:
+    test_4.write('\nfuck you')
+with open('/home/zhangxq/Documents/Python/py_study/test.txt', 'a') as test_5:
+    test_5.write('\nfuck off')
